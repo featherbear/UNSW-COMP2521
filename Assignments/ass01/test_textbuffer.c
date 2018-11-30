b84691d7b1189a526e5aa0fc3c2d4a036f142bc0
featherbear <andrew.j.wong@outlook.com>
2018-11-30 18:29:13 +1100

    ass01 | add starter code

---
+++ Assignments/ass01/test_textbuffer.c
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////////
+// COMP2521 19T0 ... assignment 1: Textbuffer ADT
+//
+// yyyy-mm-dd	Your Name Here <z5555555@unsw.edu.au>
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include "textbuffer.h"
+
+int main (void)
+{
+	// your tests go here!
+	return EXIT_SUCCESS;
+}
